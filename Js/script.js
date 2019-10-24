895198ae48ada569d3b61faa78e2c49859d16ade
JeromeMOULIN <j.moulin89@laposte.net>
2019-10-24 17:25:35 +0200

    first commit

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+let  title = document.querySelector ("h2");
+
+document.querySelector("#title").addEventListener("click", function() {
+    document.querySelector("#title").classList.toggle("title");
+});             
+
